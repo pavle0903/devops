76d11a56c0294bbfd6ff542a5c5d0f41ee56c720
Pavle9 <psarenac@gmail.com>
2023-02-06 10:02:04 -0600

    seckey

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from utils.util import kljuc
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=62!$g6i+35#wlbnjep*m(eiu1y*&wg%)qr&*g+g9z1&ic@8!w'
+SECRET_KEY = kljuc
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
