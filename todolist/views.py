e10ff2487fe5ea43b02ba633d601381238a96bdb
Pavle9 <psarenac@gmail.com>
2023-02-10 20:44:18 -0600

    edited file test

--- todolist/views.py
+++ todolist/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    text = "<h1>Welcome to my app</h1>"
+    text = "<h1>neki drugi tekst za test</h1>"
     return HttpResponse(text)
     #return render(request, "base.html", {"todo_list": todos})
